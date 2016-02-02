69668089d98d84a6b32cd9acf3ea7bf88994d52a
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-02-02 11:33:31 +0100

    Initial Commit

---
+++ FirstClass/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FirstClass
+//
+//  Created by Ignacio Lasaosa Sáez on 2/2/16.
+//  Copyright © 2016 Ignacio Lasaosa Sáez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
