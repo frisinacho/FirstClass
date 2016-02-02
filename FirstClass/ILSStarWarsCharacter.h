753e849635ff312b61e4a0ebef634ca7b58f781e
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-02-02 11:43:32 +0100

    Creating Cocoa Class

---
+++ FirstClass/ILSStarWarsCharacter.h
@@ -0,0 +1,13 @@
+//
+//  ILSStarWarsCharacter.h
+//  FirstClass
+//
+//  Created by Ignacio Lasaosa Sáez on 2/2/16.
+//  Copyright © 2016 Ignacio Lasaosa Sáez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ILSStarWarsCharacter : NSObject
+
+@end
---
+++ FirstClass/ILSStarWarsCharacter.m
@@ -0,0 +1,13 @@
+//
+//  ILSStarWarsCharacter.m
+//  FirstClass
+//
+//  Created by Ignacio Lasaosa Sáez on 2/2/16.
+//  Copyright © 2016 Ignacio Lasaosa Sáez. All rights reserved.
+//
+
+#import "ILSStarWarsCharacter.h"
+
+@implementation ILSStarWarsCharacter
+
+@end
